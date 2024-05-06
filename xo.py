c25d811ee6a0c9d0e76b850887785ca01fef0186
kirillSolovev07 <163004021+kirillSolovev07@users.noreply.github.com>
2024-05-06 16:04:48 +0500

    Second commit

--- xo.py
+++ xo.py
@@ -8,7 +8,8 @@ size_board = 3
 size_cell = 150
 margin = 25
 cell_coord_list = [(x, y) for x in range(1, 4) for y in range(1, 4)]
-val_board = [0 for _ in range(10)]
+val_board = [-1 for _ in range(10)]
+player = computer = None
 game_over = False
 
 
@@ -22,9 +23,24 @@ class Button:
         self.fill_color = fill_color
 
         self.create_button()
+        self.setText()
+        self.draw_text()
+
+    def draw_rect(self):
+        return Rect(0, 0, 500, 50)
 
     def create_button(self):
+        self.rect = self.draw_rect()
         draw.rect(scene, self.fill_color, (self.x, self.y, self.width, self.height))
+
+    def setText(self):
+        self.cr_text = font.Font(None, self.height).render(self.text, True, WHITE)
+
+    def draw_text(self):
+        width_text = self.cr_text.get_width()
+        height_text = self.cr_text.get_height()
+        scene.blit(self.cr_text,
+                   (self.x + (self.width // 2 - width_text // 2), self.y + (self.height // 2 - height_text // 2)))
 
 
 def draw_grid():
@@ -35,8 +51,8 @@ def draw_grid():
                   (margin + size_cell + size_cell * i - 1, margin + 50 + size_cell * size_board), 2)
 
 
-def create_but(start_coord, finish_coord, text):
-    return Button(finish_coord[0], finish_coord[1], start_coord[0], start_coord[1], text, BLACK)
+def create_but(width, height, start_coord, text):
+    return Button(width, height, start_coord[0], start_coord[1], text, BLACK)
 
 
 def draw_img_player(coord_cell):
@@ -45,18 +61,23 @@ def draw_img_player(coord_cell):
 
 def check_win():
     for i in range(size_board):
-        if (val_board[i * 3] == val_board[i * 3 + 1] == val_board[i * 3 + 2]) or (
-                val_board[i] == val_board[i + 3] == val_board[i + 6]) or (
-                val_board[0] == val_board[4] == val_board[8]) or (
-                val_board[2] == val_board[4] == val_board[6]):
+        print(i)
+        if (val_board[i * 3] == val_board[i * 3 + 1] == val_board[i * 3 + 2] and val_board[i * 3] + val_board[
+            i * 3 + 1] + val_board[i * 3 + 2] > 0) or (
+                val_board[i] == val_board[i + 3] == val_board[i + 6] and val_board[i] + val_board[i + 3] + val_board[
+            i + 6] > 0) or (
+                val_board[0] == val_board[4] == val_board[8] and val_board[0] + val_board[4] + val_board[8] > 0) or (
+                val_board[2] == val_board[4] == val_board[6] and val_board[2] + val_board[4] + val_board[6] > 0):
+            print(val_board)
             return True
     return False
 
 
 def step(coord_cell):
     if not check_win():
-        num_cell = coord_cell[0] ** 2 * coord_cell[1] - 1
-        val_board[num_cell] = None
+        num_cell = coord_cell[0] + (coord_cell[1] - 1) * size_board - 1
+        val_board[num_cell] = 1
+        # print(num_cell)
 
 
 init()
@@ -65,20 +86,27 @@ scene = display.set_mode(size)
 display.set_caption("Крестики-Нолики")
 scene.fill(WHITE)
 
+X = create_but(120, 40, (65, 15), "X")
+O = create_but(120, 40, (315, 15), "0")
+
 while not game_over:
+    draw_grid()
+
     for e in event.get():
         if e.type == QUIT:
             game_over = True
         elif e.type == MOUSEBUTTONDOWN:
             x, y = e.pos
             click_coord = ((x - margin) // size_cell + 1, (y - margin - 50) // size_cell + 1)
-            if margin < x < margin + size_cell * size_board and margin < y < margin + size_cell * size_board and click_coord in cell_coord_list:
+            if (
+                    margin <= x <= margin + size_cell * size_board and margin + 50 <= y <= margin + 50 + size_cell * size_board
+                    and click_coord in cell_coord_list):
                 step(click_coord)
-                cell_coord_list.remove(((x - margin) // size_cell + 1, (y - margin) // size_cell + 1))
-
-        draw_grid()
-        a = create_but((90, 15), (160, 35), "Ffff")
-        b = create_but((340, 15), (410, 35), "Ffff")
+                cell_coord_list.remove(((x - margin) // size_cell + 1, (y - (margin + 50)) // size_cell + 1))
+            if (X.x <= x <= X.x + X.width) and (X.y <= y <= X.y + X.height):
+                player, computer = 1, 0
+            elif O.x <= x <= O.x + X.width and O.y <= y <= O.y + O.height:
+                player, computer = 0, 1
 
         display.update()
         time.delay(60)
