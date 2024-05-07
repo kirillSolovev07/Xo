b4ab80a0e742fce363b0407cf72a640df142a0dd
kirillSolovev07 <163004021+kirillSolovev07@users.noreply.github.com>
2024-05-07 22:55:14 +0500

    Commit

--- xo.py
+++ xo.py
@@ -3,13 +3,19 @@ from pygame import *
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-size = (500, 550)
+size = (500, 500)
 size_board = 3
 size_cell = 150
 margin = 25
+count_step = 0
 cell_coord_list = [(x, y) for x in range(1, 4) for y in range(1, 4)]
 val_board = [-1 for _ in range(10)]
 player = computer = None
+queue = (player, computer)
+queue_pos = 0
+players = ["0", "X"]
+img_X = transform.scale(image.load("img/x.png"), (size_cell - 20, size_cell - 20))
+img_O = transform.scale(image.load("img/o.png"), (size_cell - 20, size_cell - 20))
 game_over = False
 
 
@@ -26,31 +32,30 @@ class Button:
         self.setText()
         self.draw_text()
 
-    def draw_rect(self):
-        return Rect(0, 0, 500, 50)
-
     def create_button(self):
-        self.rect = self.draw_rect()
+        #  Рисует кнопку
         draw.rect(scene, self.fill_color, (self.x, self.y, self.width, self.height))
 
     def setText(self):
+        # Устанавливает текст на кнопке
         self.cr_text = font.Font(None, self.height).render(self.text, True, WHITE)
 
     def draw_text(self):
         width_text = self.cr_text.get_width()
         height_text = self.cr_text.get_height()
+        # Отрисовка текста по центру кнопки
         scene.blit(self.cr_text,
                    (self.x + (self.width // 2 - width_text // 2), self.y + (self.height // 2 - height_text // 2)))
 
 
-def draw_grid():
+def draw_grid(offset=0):
     for i in range(size_board - 1):
         # Рисует горизонтальную линию
-        draw.line(scene, BLACK, (margin, margin + 50 + size_cell + size_cell * i - 1),
-                  (margin + size_cell * size_board, margin + 50 + size_cell + size_cell * i - 1), 2)
+        draw.line(scene, BLACK, (margin, margin + offset + size_cell + size_cell * i - 1),
+                  (margin + size_cell * size_board, margin + offset + size_cell + size_cell * i - 1), 2)
         # Рисует вертикальную линию
-        draw.line(scene, BLACK, (margin + size_cell + size_cell * i - 1, margin + 50),
-                  (margin + size_cell + size_cell * i - 1, margin + 50 + size_cell * size_board), 2)
+        draw.line(scene, BLACK, (margin + size_cell + size_cell * i - 1, margin + offset),
+                  (margin + size_cell + size_cell * i - 1, margin + offset + size_cell * size_board), 2)
 
 
 def create_but(width, height, start_coord, text):
@@ -58,63 +63,94 @@ def create_but(width, height, start_coord, text):
 
 
 def draw_img_player(coord_cell):
-    pass
+    queue_player = queue[queue_pos]
+    if queue_player:
+        scene.blit(img_X, (
+            (margin + 10) + ((coord_cell[0] - 1) * size_cell), (margin + 10) + ((coord_cell[1] - 1) * size_cell)))
+    else:
+        scene.blit(img_O, (
+            (margin + 10) + ((coord_cell[0] - 1) * size_cell), (margin + 10) + ((coord_cell[1] - 1) * size_cell)))
+
+
+def hide_button():
+    global scene
+    scene = display.set_mode(size)
+    scene.fill(WHITE)
+    draw_grid()
 
 
 def check_win():
     for i in range(size_board):
-        print(i)
         # Проверка выигрыша
         if (val_board[i * 3] == val_board[i * 3 + 1] == val_board[i * 3 + 2] and val_board[i * 3] + val_board[
-            i * 3 + 1] + val_board[i * 3 + 2] > 0) or (
+            i * 3 + 1] + val_board[i * 3 + 2] >= 0) or (
                 val_board[i] == val_board[i + 3] == val_board[i + 6] and val_board[i] + val_board[i + 3] + val_board[
-            i + 6] > 0) or (
-                val_board[0] == val_board[4] == val_board[8] and val_board[0] + val_board[4] + val_board[8] > 0) or (
-                val_board[2] == val_board[4] == val_board[6] and val_board[2] + val_board[4] + val_board[6] > 0):
-            print(val_board)
+            i + 6] >= 0) or (
+                val_board[0] == val_board[4] == val_board[8] and val_board[0] + val_board[4] + val_board[8] >= 0) or (
+                val_board[2] == val_board[4] == val_board[6] and val_board[2] + val_board[4] + val_board[
+            6] >= 0) or count_step == 9:
             return True
     return False
 
 
+def finish_screen(text):
+    obj_text = font.Font(None, 100).render(text, True, BLACK)
+    scene.fill(WHITE)
+    scene.blit(obj_text, (size[0] // 2 - (obj_text.get_width() // 2), size[1] // 2 - (obj_text.get_height() // 2)))
+
+
 def step(coord_cell):
-    if not check_win():
-        num_cell = coord_cell[0] + (coord_cell[1] - 1) * size_board - 1 # Номер нажатой ячейки
-        val_board[num_cell] = 1 # Ход игрока, добавление хода в val_board
-        # print(num_cell)
+    global queue_pos, count_step
+    num_cell = coord_cell[0] + (coord_cell[1] - 1) * size_board - 1  # Номер нажатой ячейки
+    val_board[num_cell] = queue[queue_pos]  # Ход игрока, добавление хода в val_board
+    draw_img_player(coord_cell)
+    count_step += 1
+    if check_win():
+        if count_step == 9:
+            scene.fill(BLACK)
+        else:
+            finish_screen(f"Победил {players[queue[queue_pos]]}")
+    queue_pos = 1 - queue_pos
 
 
 init()
 
-scene = display.set_mode(size)
+scene = display.set_mode((size[0], size[1] + 50))
 display.set_caption("Крестики-Нолики")
+display.set_icon(image.load("img/icon.ico"))
 scene.fill(WHITE)
 
 # Создание объектов Крестика и Нолика
-X = create_but(120, 40, (65, 15), "X")
-O = create_but(120, 40, (315, 15), "0")
+X_player = create_but(120, 40, (65, 15), "X")
+O_player = create_but(120, 40, (315, 15), "0")
+
+draw_grid(offset=50)  # Добавление смещения
 
 while not game_over:
-    draw_grid()
+    queue = (player, computer)
 
     for e in event.get():
         if e.type == QUIT:
             game_over = True
         elif e.type == MOUSEBUTTONDOWN:
             x, y = e.pos
-            click_coord = ((x - margin) // size_cell + 1, (y - margin - 50) // size_cell + 1) # Координаты клика
-            # Проверка, что клик не выходит за пределы сетки
+            click_coord = ((x - margin) // size_cell + 1, (y - margin) // size_cell + 1)  # Координаты клика
+            # Проверка, что клик не выходит за пределы сетки и выбран X либо 0
             if (
-                    margin <= x <= margin + size_cell * size_board and margin + 50 <= y <= margin + 50 + size_cell * size_board
-                    and click_coord in cell_coord_list):
-                step(click_coord)
-                cell_coord_list.remove(((x - margin) // size_cell + 1, (y - (margin + 50)) // size_cell + 1)) # Удаление нажатых координат
+                    margin <= x <= margin + size_cell * size_board and margin <= y <= margin + size_cell * size_board
+                    and click_coord in cell_coord_list and player != computer != None):
+                step(click_coord)  # Совершение хода
+                cell_coord_list.remove(click_coord)  # Удаление нажатых координат
             # Проверка,что клик по кнопке X
-            if (X.x <= x <= X.x + X.width) and (X.y <= y <= X.y + X.height):
+            if (X_player.x <= x <= X_player.x + X_player.width) and (X_player.y <= y <= X_player.y + X_player.height):
                 # 1 - крестик; 0 - нолик
                 player, computer = 1, 0
+                hide_button()
             # Проверка, что клик по кнопке 0
-            elif O.x <= x <= O.x + X.width and O.y <= y <= O.y + O.height:
+            elif O_player.x <= x <= O_player.x + X_player.width and O_player.y <= y <= O_player.y + O_player.height:
+                # 1 - крестик; 0 - нолик
                 player, computer = 0, 1
+                hide_button()
 
         display.update()
         time.delay(60)
